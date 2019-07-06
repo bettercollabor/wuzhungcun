e0c43d34f1e68db91d99130e3981b12431300f68
kerrychang1987 <zhaileisanguo@163.com>
2019-07-06 11:18:13 +0800

    提交代码

--- src/com/zhailei/git/FirstGit.java
+++ src/com/zhailei/git/FirstGit.java
@@ -5,5 +5,9 @@ public class FirstGit {
 	public static void main(String[] args) {
 		System.out.println("qqqqqqqqqqq");
 	}
+	
+	public static void add() {
+		System.out.println("aaaaaaaaaaaaa");
+	}
 
 }
