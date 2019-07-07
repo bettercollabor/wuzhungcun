4bca78db904e77992aa997a45cfc2e2be628561e
kerrychang1987 <zhaileisanguo@163.com>
2019-07-07 15:28:39 +0800

    再次提交代码

--- src/com/zhailei/git/FirstGit.java
+++ src/com/zhailei/git/FirstGit.java
@@ -14,7 +14,16 @@ public class FirstGit {
 		System.out.println("aaaaaaaaaaaaa");
 	}
 	
-	public static void add() {
+    
+	public static void addadd() {
+		System.out.println("aaaaaaaaaaaaa");
+	}
+	
+	public static void multify() {
+		System.out.println("aaaaaaaaaaaaa");
+	}
+	
+	public static void add2() {
 		System.out.println("aaaaaaaaaaaaa");
 	}
 
